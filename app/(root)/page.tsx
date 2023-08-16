af6f6011170ffacf63be168c4c949844e6b7affe
Luis <luish.vargas09@gmail.com>
2023-08-16 02:46:45 -0600

    Modal components (Admin)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,23 @@
-import { UserButton } from '@clerk/nextjs';
+'use client';
 
+import { useEffect } from 'react';
+import { useStoreModal } from '@/hooks/use-store-modal';
+
+// Home Page
 const SetUpPage = () => {
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect(() => {
+        if (!isOpen) {
+            onOpen();
+        }
+    }, [isOpen, onOpen]);
+
     return (
-        <div className='flex justify-end p-5 mx-5'>
-            <UserButton afterSignOutUrl='/' />
-        </div>
+        <>
+            <div className='flex justify-end p-5 mx-5'>Root Page</div>
+        </>
     );
 };
 
